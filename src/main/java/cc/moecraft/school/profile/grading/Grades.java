d08e695802160f3427d86ec9c2dc6990651ca95a
Hykilpikonna <admin@moecraft.cc>
2018-10-05 14:07:10 -0400

    [+] Add grade enum

---
+++ src/main/java/cc/moecraft/school/profile/grading/Grades.java
@@ -0,0 +1,37 @@
+package cc.moecraft.school.profile.grading;
+
+import lombok.Getter;
+import lombok.ToString;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/05 创建!
+ * Created by Hykilpikonna on 2018/10/05!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@Getter
+public enum Grades
+{
+    A_PLUS  ("A+"),
+    A       ("A"),
+    A_MINUS ("A-"),
+    B_PLUS  ("B+"),
+    B       ("B"),
+    B_MINUS ("B-"),
+    C_PLUS  ("C+"),
+    C       ("C"),
+    C_MINUS ("C-"),
+    D_PLUS  ("D+"),
+    D       ("D"),
+    D_MINUS ("D-"),
+    FAIL    ("F");
+
+    private String name;
+
+    Grades(String name, String... alias)
+    {
+        this.name = name;
+    }
+}
