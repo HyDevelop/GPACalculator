ac2c353b07c9d013232047d70fa7e7bda2841157
Hykilpikonna <admin@moecraft.cc>
2018-10-04 20:46:54 -0400

    [+] Course level list class

---
+++ src/main/java/cc/moecraft/school/profile/grading/CourseLevelList.java
@@ -0,0 +1,16 @@
+package cc.moecraft.school.profile.grading;
+
+import java.util.ArrayList;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/04 创建!
+ * Created by Hykilpikonna on 2018/10/04!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class CourseLevelList
+{
+    private final ArrayList<CourseLevel> levels = new ArrayList<>();
+}
