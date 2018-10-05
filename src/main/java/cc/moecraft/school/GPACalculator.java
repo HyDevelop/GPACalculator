2d8c9511f2fef484ddefca3e3350815b4cfaf59b
Hykilpikonna <admin@moecraft.cc>
2018-10-04 20:02:07 -0400

    [+] Create Main class

---
+++ src/main/java/cc/moecraft/school/GPACalculator.java
@@ -0,0 +1,14 @@
+package cc.moecraft.school;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/04 创建!
+ * Created by Hykilpikonna on 2018/10/04!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class GPACalculator
+{
+
+}
