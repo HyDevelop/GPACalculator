082e9c5b99f2ab91a898acbfb863f7e56a25444c
Hykilpikonna <admin@moecraft.cc>
2018-10-05 19:23:34 -0400

    [+] Create GradingProfile class

---
+++ src/main/java/cc/moecraft/school/profile/grading/GradingProfile.java
@@ -0,0 +1,15 @@
+package cc.moecraft.school.profile.grading;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/04 创建!
+ * Created by Hykilpikonna on 2018/10/04!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class GradingProfile
+{
+    private CourseLevelList courseLevelList;
+    private double gradeDecreaseBy;
+}
