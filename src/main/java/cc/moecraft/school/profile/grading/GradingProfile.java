e66922035a5ca933eb1c01b4ca596bf321034c7b
Hykilpikonna <admin@moecraft.cc>
2018-10-05 19:23:59 -0400

    [+] Data lombok annotation

--- src/main/java/cc/moecraft/school/profile/grading/GradingProfile.java
+++ src/main/java/cc/moecraft/school/profile/grading/GradingProfile.java
@@ -1,5 +1,8 @@
 package cc.moecraft.school.profile.grading;
 
+import cc.moecraft.yaml.HyConfig;
+import lombok.Data;
+
 /**
  * 此类由 Hykilpikonna 在 2018/10/04 创建!
  * Created by Hykilpikonna on 2018/10/04!
@@ -8,6 +11,7 @@ package cc.moecraft.school.profile.grading;
  *
  * @author Hykilpikonna
  */
+@Data
 public class GradingProfile
 {
     private CourseLevelList courseLevelList;
