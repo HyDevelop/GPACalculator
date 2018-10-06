8e285e000932d3cc9fc4be6300a6fc235d0988fe
Hykilpikonna <admin@moecraft.cc>
2018-10-06 09:38:01 -0400

    [+] Not Found exception

---
+++ src/main/java/cc/moecraft/school/NotFoundException.java
@@ -0,0 +1,20 @@
+package cc.moecraft.school;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/06 创建!
+ * Created by Hykilpikonna on 2018/10/06!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data @AllArgsConstructor
+public class NotFoundException extends Exception
+{
+    private String message;
+}
