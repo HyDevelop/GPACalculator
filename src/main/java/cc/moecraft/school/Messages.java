7126caefbb576869c1928c40139f99acba571b77
Hykilpikonna <admin@moecraft.cc>
2018-10-06 09:38:15 -0400

    [+] Messages

---
+++ src/main/java/cc/moecraft/school/Messages.java
@@ -0,0 +1,16 @@
+package cc.moecraft.school;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/06 创建!
+ * Created by Hykilpikonna on 2018/10/06!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class Messages
+{
+    public static final String SUBJECT_NOT_FOUND = "Subject %s not found in student profile.";
+    public static final String LEVEL_NOT_FOUND = "Course level %s specified in student profile not found.";
+    public static final String LETTER_GRADE_NOT_FOUND = "Grade letter %s not found in the course level profile.";
+}
