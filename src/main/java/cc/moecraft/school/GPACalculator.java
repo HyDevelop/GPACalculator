0c464e85d553e3960906476d68d725b8fb6b5883
Hykilpikonna <admin@moecraft.cc>
2018-10-06 09:38:46 -0400

    [+] Verify notnull and throw notfound

--- src/main/java/cc/moecraft/school/GPACalculator.java
+++ src/main/java/cc/moecraft/school/GPACalculator.java
@@ -1,5 +1,16 @@
 package cc.moecraft.school;
 
+import cc.moecraft.school.profile.grading.CLGradeWeight;
+import cc.moecraft.school.profile.grading.CourseLevel;
+import cc.moecraft.school.profile.grading.GradingProfile;
+import cc.moecraft.school.profile.student.StudentProfile;
+import cc.moecraft.school.profile.student.Subject;
+
+import java.util.ArrayList;
+import java.util.Map;
+
+import static cc.moecraft.school.Messages.*;
+
 /**
  * 此类由 Hykilpikonna 在 2018/10/04 创建!
  * Created by Hykilpikonna on 2018/10/04!
@@ -11,4 +22,16 @@ package cc.moecraft.school;
 public class GPACalculator
 {
 
+    /**
+     * Verify if a object is null or not. Throw exception if it is.
+     *
+     * @param object Object to verify
+     * @param f Format of the message
+     * @param args Message arguments
+     * @throws NotFoundException Is null
+     */
+    private static void notNull(Object object, String f, Object ... args) throws NotFoundException
+    {
+        if (object == null) throw new NotFoundException(String.format(f, args));
+    }
 }
