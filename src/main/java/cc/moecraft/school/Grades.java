c7bee6bfd92dd100bcadfdb6d27410118dda6a43
Hykilpikonna <admin@moecraft.cc>
2018-10-06 09:17:02 -0400

    [+] Grades class

---
+++ src/main/java/cc/moecraft/school/Grades.java
@@ -0,0 +1,42 @@
+package cc.moecraft.school;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/06 创建!
+ * Created by Hykilpikonna on 2018/10/06!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@Data
+public class Grades
+{
+    /**
+     * Previous GPA
+     */
+    private double previousGPA;
+
+    /**
+     * Subject to grade.
+     * e.g. "English 2 A", "A-"
+     */
+    private final Map<String, String> subjectGradeMap = new HashMap<>();
+
+    /**
+     * Put a grade in the map
+     *
+     * @param subject Subject name
+     * @param grade Grade letter
+     */
+    public void putGrade(String subject, String grade)
+    {
+        subjectGradeMap.put(subject, grade);
+    }
+}
