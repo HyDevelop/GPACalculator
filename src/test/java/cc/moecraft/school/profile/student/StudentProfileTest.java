0dd37706a440140a8f5d121a32368985758f5e8d
Hykilpikonna <admin@moecraft.cc>
2018-10-06 09:03:04 -0400

    [T] Test student profile

---
+++ src/test/java/cc/moecraft/school/profile/student/StudentProfileTest.java
@@ -0,0 +1,25 @@
+package cc.moecraft.school.profile.student;
+
+import cc.moecraft.school.profile.grading.GradingProfile;
+import cc.moecraft.yaml.HyConfig;
+
+import java.io.File;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/06 创建!
+ * Created by Hykilpikonna on 2018/10/06!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class StudentProfileTest
+{
+    public static void main(String[] args)
+    {
+        HyConfig config = new HyConfig(new File("./profiles/student/Example.yml"));
+        StudentProfile exampleProfile = StudentProfile.parseFromConfig(config, "");
+
+        System.out.println(exampleProfile);
+    }
+}
