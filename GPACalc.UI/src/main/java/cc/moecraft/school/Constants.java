248cd38fe10bb80db16a2f13964aa92c4d10327a
Hykilpikonna <admin@moecraft.cc>
2018-10-06 16:21:09 -0400

    [+] Add google client id

---
+++ GPACalc.UI/src/main/java/cc/moecraft/school/Constants.java
@@ -0,0 +1,14 @@
+package cc.moecraft.school;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/06 创建!
+ * Created by Hykilpikonna on 2018/10/06!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class Constants
+{
+    public static final String GOOGLE_CLIENT_ID = "1003130608774-b3lpkuuk73f07o9kgsve3mp1b30ban7v";
+}
