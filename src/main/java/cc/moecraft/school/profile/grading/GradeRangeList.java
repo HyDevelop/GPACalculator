42b18e7c0f454a8fb64b94a8450e63954ddb72c2
Hykilpikonna <admin@moecraft.cc>
2018-10-06 08:05:34 -0400

    [+] Grade range list class

---
+++ src/main/java/cc/moecraft/school/profile/grading/GradeRangeList.java
@@ -0,0 +1,20 @@
+package cc.moecraft.school.profile.grading;
+
+import lombok.Getter;
+import lombok.ToString;
+
+import java.util.ArrayList;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/06 创建!
+ * Created by Hykilpikonna on 2018/10/06!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@Getter @ToString
+public class GradeRangeList
+{
+    private final ArrayList<GradeRange> gradeRanges = new ArrayList<>();
+}
