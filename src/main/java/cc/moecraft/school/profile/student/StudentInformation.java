ddc7520db91a4a277a6a1c2a696140cd6586d049
Hykilpikonna <admin@moecraft.cc>
2018-10-06 08:55:35 -0400

    [+] Student basic information class

---
+++ src/main/java/cc/moecraft/school/profile/student/StudentInformation.java
@@ -0,0 +1,31 @@
+package cc.moecraft.school.profile.student;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/06 创建!
+ * Created by Hykilpikonna on 2018/10/06!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@AllArgsConstructor @Data
+public class StudentInformation
+{
+    /**
+     * Student name
+     */
+    private String name;
+
+    /**
+     * Grade level. (eg. Junior = 11)
+     */
+    private short grade;
+
+    /**
+     * Semester (1 or 2)
+     */
+    private short semester;
+}
