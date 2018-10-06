8a8776318505945327a510ba3104d0ab2b68d8e2
Hykilpikonna <admin@moecraft.cc>
2018-10-06 08:14:25 -0400

    [+] Add CLGradeWeight and it's list

---
+++ src/main/java/cc/moecraft/school/profile/grading/CLGradeWeightList.java
@@ -0,0 +1,13 @@
+package cc.moecraft.school.profile.grading;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/06 创建!
+ * Created by Hykilpikonna on 2018/10/06!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class CLGradeWeightList
+{
+}
