98b26d3e7ae482e2df9258f243d339efe65d1bd1
Hykilpikonna <admin@moecraft.cc>
2018-10-06 08:04:02 -0400

    [+] Grade range class

---
+++ src/main/java/cc/moecraft/school/profile/grading/GradeRange.java
@@ -0,0 +1,31 @@
+package cc.moecraft.school.profile.grading;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/06 创建!
+ * Created by Hykilpikonna on 2018/10/06!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Data
+public class GradeRange
+{
+    /**
+     * Letter name of the grade.
+     * Example: "A+"
+     */
+    private String name;
+
+    /**
+     * This value represents the minimum amount in the range.
+     * For example, A+ would be 97 - 100, so put 97 as the value.
+     */
+    private double range;
+}
