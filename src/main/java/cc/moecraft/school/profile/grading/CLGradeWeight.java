36e4796cddb411369e860a8679bdb9547f6309ea
Hykilpikonna <admin@moecraft.cc>
2018-10-06 08:17:11 -0400

    [+] Declare instance fields

--- src/main/java/cc/moecraft/school/profile/grading/CLGradeWeight.java
+++ src/main/java/cc/moecraft/school/profile/grading/CLGradeWeight.java
@@ -1,5 +1,8 @@
 package cc.moecraft.school.profile.grading;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
 /**
  * 此类由 Hykilpikonna 在 2018/10/06 创建!
  * Created by Hykilpikonna on 2018/10/06!
@@ -8,6 +11,19 @@ package cc.moecraft.school.profile.grading;
  *
  * @author Hykilpikonna
  */
+@AllArgsConstructor
+@Data
 public class CLGradeWeight
 {
+    /**
+     * Letter name of the grade.
+     * Example: "A+"
+     */
+    private String name;
+
+    /**
+     * The amount of points you get for this grade.
+     * For example, "A+" = 5.0, "A" = 4.75
+     */
+    private double credit;
 }
