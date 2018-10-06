18261e95b0fdb2e0d2ff2d4c4356122fd7742fae
Hykilpikonna <admin@moecraft.cc>
2018-10-06 11:07:38 -0400

    [+] String utils

---
+++ src/main/java/cc/moecraft/school/utils/StringUtils.java
@@ -0,0 +1,41 @@
+package cc.moecraft.school.utils;
+
+import static java.lang.Double.parseDouble;
+import static java.lang.Integer.parseInt;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/06 创建!
+ * Created by Hykilpikonna on 2018/10/06!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class StringUtils
+{
+    public static boolean isInt(String string)
+    {
+        try
+        {
+            int x = parseInt(string);
+            return true;
+        }
+        catch (NumberFormatException e)
+        {
+            return false;
+        }
+    }
+
+    public static boolean isDouble(String string)
+    {
+        try
+        {
+            double x = parseDouble(string);
+            return true;
+        }
+        catch (NumberFormatException e)
+        {
+            return false;
+        }
+    }
+}
