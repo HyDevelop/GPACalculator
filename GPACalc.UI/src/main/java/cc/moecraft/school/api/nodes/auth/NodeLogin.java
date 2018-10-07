1d78530db025b836c061eab211c2051918038f4f
Hykilpikonna <admin@moecraft.cc>
2018-10-07 12:33:14 -0400

    [+] API node for login and registration

---
+++ GPACalc.UI/src/main/java/cc/moecraft/school/api/nodes/auth/NodeLogin.java
@@ -0,0 +1,44 @@
+package cc.moecraft.school.api.nodes.auth;
+
+import cc.moecraft.school.Constants;
+import cc.moecraft.school.api.ApiNode;
+import cc.moecraft.school.database.model.UserProfiles;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/06 创建!
+ * Created by Hykilpikonna on 2018/10/06!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class NodeLogin implements ApiNode
+{
+    @Override
+    public String nodeName()
+    {
+        return "auth.login";
+    }
+
+    @Override
+    public String process(HttpServletRequest request, GoogleIdToken token)
+    {
+        GoogleIdToken.Payload id = token.getPayload();
+
+        UserProfiles profiles = UserProfiles.service().findById(id.getSubject());
+        if (profiles == null)
+        {
+            // Register
+            profiles = new UserProfiles();
+            profiles.setGoogleSub(id.getSubject());
+            profiles.setStudentProfile(Constants.DEFAULT_STUDENT_PROFILE);
+            profiles.setGradingProfile(Constants.DEFAULT_GRADING_PROFILE);
+            return "Registered";
+        }
+
+        return "Logged in";
+    }
+}
