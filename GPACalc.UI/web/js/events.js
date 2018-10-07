bc300760d735ba41438d9a2dcaab2974c42a8e40
Hykilpikonna <admin@moecraft.cc>
2018-10-07 18:42:03 -0400

    [+] Detect numeric

--- GPACalc.UI/web/js/events.js
+++ GPACalc.UI/web/js/events.js
@@ -1,1 +1,14 @@
 
+function checkNumeric(id)
+{
+    var element = $("#" + id);
+    var value = element.val();
+    if (isNaN(value))
+    {
+        element.parent().addClass("error");
+    }
+    else
+    {
+        element.parent().removeClass("error");
+    }
+}
