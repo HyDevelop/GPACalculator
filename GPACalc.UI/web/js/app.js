8ec91aa7131fd45c6b402a1cdf09b999e93fb66f
Hykilpikonna <admin@moecraft.cc>
2018-10-07 14:10:54 -0400

    [+] Reload on sign off

--- GPACalc.UI/web/js/app.js
+++ GPACalc.UI/web/js/app.js
@@ -36,6 +36,19 @@ function onSignIn(user)
 }
 
 /**
+ * Sign out.
+ */
+function signOut()
+{
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function ()
+    {
+        console.log('User signed out.');
+        reload();
+    });
+}
+
+/**
  * Reload page.
  */
 function reload()
