9228231b7c3a857f73a99183a1a90413f54dd52c
Hykilpikonna <admin@moecraft.cc>
2018-10-07 08:12:17 -0400

    [+] Load secret from runtime config

--- GPACalc.UI/src/main/java/cc/moecraft/school/Constants.java
+++ GPACalc.UI/src/main/java/cc/moecraft/school/Constants.java
@@ -1,5 +1,9 @@
 package cc.moecraft.school;
 
+import cc.moecraft.yaml.HyConfig;
+
+import java.io.File;
+
 /**
  * 此类由 Hykilpikonna 在 2018/10/06 创建!
  * Created by Hykilpikonna on 2018/10/06!
@@ -10,5 +14,22 @@ package cc.moecraft.school;
  */
 public class Constants
 {
-    public static final String GOOGLE_CLIENT_ID = "1003130608774-b3lpkuuk73f07o9kgsve3mp1b30ban7v.apps.googleusercontent.com";
+    public static final File RUNTIME_CONFIG_FILE = new File("./runtime-config.yml");
+    public static final HyConfig RUNTIME_CONFIG;
+
+    public static final String GOOGLE_CLIENT_ID;
+    public static final String JDBC_CONNECTION;
+
+    public static final String USERNAME;
+    public static final String PASSWORD;
+
+    static
+    {
+        RUNTIME_CONFIG = new HyConfig(RUNTIME_CONFIG_FILE);
+
+        GOOGLE_CLIENT_ID = RUNTIME_CONFIG.getString("ClientID");
+        JDBC_CONNECTION = RUNTIME_CONFIG.getString("Database.JDBConnection");
+        USERNAME = RUNTIME_CONFIG.getString("Database.Username");
+        PASSWORD = RUNTIME_CONFIG.getString("Database.Password");
+    }
 }
