8916207d5eddae75f92637fce64951015253d967
Hykilpikonna <admin@moecraft.cc>
2018-10-07 18:42:14 -0400

    [+] Detect A-Za-z

--- GPACalc.UI/web/js/events.js
+++ GPACalc.UI/web/js/events.js
@@ -12,3 +12,17 @@ function checkNumeric(id)
         element.parent().removeClass("error");
     }
 }
+
+function checkAZ(id)
+{
+    var element = $("#" + id);
+    var value = element.val();
+    if (/[a-zA-Z]*/.test(value))
+    {
+        element.parent().addClass("error");
+    }
+    else
+    {
+        element.parent().removeClass("error");
+    }
+}
