5bb4b9eb6e17ba683c7cdef243c7ae20810ef0ae
Hykilpikonna <admin@moecraft.cc>
2018-10-07 14:07:17 -0400

    [+] Test node

---
+++ GPACalc.UI/src/main/java/cc/moecraft/school/api/nodes/misc/NodeTest.java
@@ -0,0 +1,29 @@
+package cc.moecraft.school.api.nodes.misc;
+
+import cc.moecraft.school.api.ApiNode;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/07 创建!
+ * Created by Hykilpikonna on 2018/10/07!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class NodeTest implements ApiNode
+{
+    @Override
+    public String nodeName()
+    {
+        return "misc.test";
+    }
+
+    @Override
+    public String process(HttpServletRequest request, GoogleIdToken token)
+    {
+        return "Success! " + token;
+    }
+}
