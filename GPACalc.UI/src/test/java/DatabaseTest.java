c25405c13cecbdc5f308c9659cb071ae6a470c42
Hykilpikonna <admin@moecraft.cc>
2018-10-07 12:04:49 -0400

    [T] Test database

---
+++ GPACalc.UI/src/test/java/DatabaseTest.java
@@ -0,0 +1,31 @@
+import cc.moecraft.school.database.DatabaseInitializer;
+import cc.moecraft.school.database.model.UserProfiles;
+import cc.moecraft.school.database.service.UserProfileService;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/07 创建!
+ * Created by Hykilpikonna on 2018/10/07!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class DatabaseTest
+{
+    public static void main(String[] args)
+    {
+        // Initialize
+        DatabaseInitializer initializer = new DatabaseInitializer();
+        initializer.contextInitialized(null);
+
+        // Create an entry
+        UserProfiles profiles = new UserProfiles();
+        profiles.setGoogleSub("test");
+        profiles.setGradingProfile("A: B");
+        profiles.setStudentProfile("A: B");
+        profiles.save();
+
+        // Find it
+        System.out.println(new UserProfileService().findById("test"));
+    }
+}
