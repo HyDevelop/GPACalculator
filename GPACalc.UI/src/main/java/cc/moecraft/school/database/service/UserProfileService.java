b62071f5bcb491d5ba844a82f81503482c878973
Hykilpikonna <admin@moecraft.cc>
2018-10-07 12:04:27 -0400

    [+] Create service class

---
+++ GPACalc.UI/src/main/java/cc/moecraft/school/database/service/UserProfileService.java
@@ -0,0 +1,17 @@
+package cc.moecraft.school.database.service;
+
+import cc.moecraft.school.database.model.UserProfiles;
+import io.jboot.JbootServiceBase;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/07 创建!
+ * Created by Hykilpikonna on 2018/10/07!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class UserProfileService extends JbootServiceBase<UserProfiles>
+{
+
+}
