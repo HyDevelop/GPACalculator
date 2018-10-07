7c620f90acb01d9ac78a9a12500e438f61d8c6ad
Hykilpikonna <admin@moecraft.cc>
2018-10-07 12:30:42 -0400

    [+] Default profile

--- GPACalc.UI/src/main/java/cc/moecraft/school/Constants.java
+++ GPACalc.UI/src/main/java/cc/moecraft/school/Constants.java
@@ -3,6 +3,9 @@ package cc.moecraft.school;
 import cc.moecraft.yaml.HyConfig;
 
 import java.io.File;
+import java.io.IOException;
+
+import static org.apache.commons.io.FileUtils.readFileToString;
 
 /**
  * 此类由 Hykilpikonna 在 2018/10/06 创建!
@@ -23,6 +26,9 @@ public class Constants
     public static final String USERNAME;
     public static final String PASSWORD;
 
+    public static final String DEFAULT_STUDENT_PROFILE;
+    public static final String DEFAULT_GRADING_PROFILE;
+
     static
     {
         RUNTIME_CONFIG = new HyConfig(RUNTIME_CONFIG_FILE);
@@ -31,5 +37,15 @@ public class Constants
         JDBC_CONNECTION = RUNTIME_CONFIG.getString("Database.JDBConnection");
         USERNAME = RUNTIME_CONFIG.getString("Database.Username");
         PASSWORD = RUNTIME_CONFIG.getString("Database.Password");
+
+        try
+        {
+            DEFAULT_GRADING_PROFILE = readFileToString(new File(RUNTIME_CONFIG.getString("DefaultProfile.Grading")), "utf-8");
+            DEFAULT_STUDENT_PROFILE = readFileToString(new File(RUNTIME_CONFIG.getString("DefaultProfile.Student")), "utf-8");
+        }
+        catch (IOException e)
+        {
+            throw new RuntimeException(e);
+        }
     }
 }
