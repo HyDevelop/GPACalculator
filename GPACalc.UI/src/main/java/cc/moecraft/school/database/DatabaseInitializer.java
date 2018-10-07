46d8501009f9100041a8cdec18cd9a08e8c18a79
Hykilpikonna <admin@moecraft.cc>
2018-10-07 08:17:50 -0400

    [+] Initialize ActiveRecord database conn

---
+++ GPACalc.UI/src/main/java/cc/moecraft/school/database/DatabaseInitializer.java
@@ -0,0 +1,39 @@
+package cc.moecraft.school.database;
+
+import cc.moecraft.school.Constants;
+import com.jfinal.plugin.activerecord.ActiveRecordPlugin;
+import com.jfinal.plugin.c3p0.C3p0Plugin;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import static cc.moecraft.school.Constants.*;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/07 创建!
+ * Created by Hykilpikonna on 2018/10/07!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class DatabaseInitializer implements ServletContextListener
+{
+    public static C3p0Plugin c3p0Plugin;
+    public static ActiveRecordPlugin activeRecordPlugin;
+
+    @Override
+    public void contextDestroyed(ServletContextEvent event)
+    {
+
+    }
+
+    @Override
+    public void contextInitialized(ServletContextEvent event)
+    {
+        c3p0Plugin = new C3p0Plugin(JDBC_CONNECTION, USERNAME, PASSWORD);
+        c3p0Plugin.start();
+        activeRecordPlugin = new ActiveRecordPlugin(c3p0Plugin);
+        activeRecordPlugin.start();
+    }
+}
