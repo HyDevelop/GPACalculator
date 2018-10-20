b99a66d5fc26aeda54f022974b11f95bcd68ae29
Hykilpikonna <admin@moecraft.cc>
2018-10-20 13:38:43 -0400

    [M] Move sign out method to utils.js

--- GPACalc.UI/web/js/app.js
+++ GPACalc.UI/web/js/app.js
@@ -72,19 +72,6 @@ function onSignIn(user)
 }
 
 /**
- * Sign out.
- */
-function signOut()
-{
-    var auth2 = gapi.auth2.getAuthInstance();
-    auth2.signOut().then(function ()
-    {
-        console.log('User signed out.');
-        reload();
-    });
-}
-
-/**
  * Run on load
  */
 function onLoad()
