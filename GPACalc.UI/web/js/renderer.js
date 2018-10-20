d44f2b1b2572a00e32520f598846ed4d0ab58642
Hykilpikonna <admin@moecraft.cc>
2018-10-20 09:45:15 -0400

    [+] Render course settings from server

--- GPACalc.UI/web/js/renderer.js
+++ GPACalc.UI/web/js/renderer.js
@@ -3,25 +3,48 @@ const editorBaseUrl = "http://" + document.domain + "/includes/editor/";
 var studentProfileEditorTemplate;
 
 /**
- * Get student profile editor template.
+ * Load student profile editor template.
  *
- * @param callback Call this when complete.
+ * @param callback Callback when done
  */
-function getStudentProfileEditorTemplate(callback)
+function loadStudentProfileEditorTemplate(callback)
 {
-    if (studentProfileEditorTemplate == null)
+    $.ajax({
+        url: editorBaseUrl + "student-profile-editor-template.jsp",
+        success: function (result)
+        {
+            studentProfileEditorTemplate = result;
+            callback();
+        }
+    });
+}
+
+/**
+ * Render the course settings page.
+ *
+ * @param json Student profile json
+ */
+function renderCourseSettingsPage(json)
+{
+    // Get div and delete all child.
+    var div = $("#student-profile-editor-div");
+    div.empty();
+
+    // Get subjects.
+    var subjects = json.subjectList.subjects;
+    for (var i in subjects)
     {
-        $.ajax({
-            url: editorBaseUrl + "student-profile-editor-template.jsp",
-            success: function (result)
-            {
-                studentProfileEditorTemplate = result;
-                getStudentProfileEditorTemplate(callback);
-            }
-        });
-    }
-    else
-    {
-        callback(studentProfileEditorTemplate);
+        var subject = subjects[i];
+        var keyName = subject.keyName;
+        var name = subject.name;
+        var level = subject.level;
+        var credits = subject.credits;
+
+        div.append(studentProfileEditorTemplate.replace(/%id%/g, i));
+
+        $("#key-" + i).val(keyName);
+        $("#name-" + i).val(name);
+        $("#level-" + i).val(level);
+        $("#credits-" + i).val(credits);
     }
 }
