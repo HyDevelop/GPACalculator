41ad7879b83203c24117c74dce5ec1a1dc8725cc
Hykilpikonna <admin@moecraft.cc>
2018-10-23 17:35:43 -0400

    [+] Veracross reader

---
+++ GPACalc.Core/src/main/java/cc/moecraft/school/veracross/VeracrossReader.java
@@ -0,0 +1,48 @@
+package cc.moecraft.school.veracross;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/05 创建!
+ * Created by Hykilpikonna on 2018/10/05!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@AllArgsConstructor
+public class VeracrossReader
+{
+    private final String username;
+    private final String password;
+
+    @Setter @Getter
+    private WebDriver webDriver;
+
+    /**
+     * Initialize with default driver (Chrome driver)
+     */
+    public void initialize()
+    {
+        initialize(new ChromeDriver());
+    }
+
+    /**
+     * Initialize with a specific web driver object.
+     *
+     * @param webDriver Selenium web driver object.
+     */
+    public void initialize(WebDriver webDriver)
+    {
+        setWebDriver(webDriver);
+    }
+
+    public void login()
+    {
+
+    }
+}
