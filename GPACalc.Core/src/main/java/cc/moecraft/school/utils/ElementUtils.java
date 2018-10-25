470c7389904402a127d2e0bed492aa8e2088a08f
Hykilpikonna <admin@moecraft.cc>
2018-10-25 17:53:25 -0400

    [+] Element Utils

---
+++ GPACalc.Core/src/main/java/cc/moecraft/school/utils/ElementUtils.java
@@ -0,0 +1,23 @@
+package cc.moecraft.school.utils;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.openqa.selenium.By;
+import org.openqa.selenium.SearchContext;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.RemoteWebElement;
+
+import java.util.List;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/25 创建!
+ * Created by Hykilpikonna on 2018/10/25!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class ElementUtils
+{
+}
