7cb0deac836abfed3233e40f2e01037726a7ec2f
Hykilpikonna <admin@moecraft.cc>
2018-10-25 18:34:27 -0400

    [+] Veracross Course Grade class

---
+++ GPACalc.Core/src/main/java/cc/moecraft/school/veracross/VeracrossCourseGrade.java
@@ -0,0 +1,20 @@
+package cc.moecraft.school.veracross;
+
+import lombok.Data;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/25 创建!
+ * Created by Hykilpikonna on 2018/10/25!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+@Data
+public class VeracrossCourseGrade
+{
+    private String name;
+    private String teacherName;
+    private String letterGrade;
+    private double numericGrade;
+}
