0cce85f9598b0bf69074d8358e70a2b8d2d42478
Hykilpikonna <admin@moecraft.cc>
2018-10-27 16:23:41 -0400

    [+] Database Utils

---
+++ GPACalc.UI/src/main/java/cc/moecraft/school/database/DatabaseUtils.java
@@ -0,0 +1,24 @@
+package cc.moecraft.school.database;
+
+import cc.moecraft.school.Constants;
+import cc.moecraft.school.database.model.UserInfo;
+import cc.moecraft.school.database.model.UserProfiles;
+import cc.moecraft.school.database.service.Services;
+import cc.moecraft.school.profile.student.StudentProfile;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+
+import static cc.moecraft.school.Constants.*;
+import static cc.moecraft.school.utils.JsonUtils.toJson;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/27 创建!
+ * Created by Hykilpikonna on 2018/10/27!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class DatabaseUtils
+{
+
+}
