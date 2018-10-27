3ed3c1ecb2302bf7b8408f8e38a6e3f66c3fb93a
Hykilpikonna <admin@moecraft.cc>
2018-10-27 16:52:41 -0400

    [M] Move entry order

--- GPACalc.UI/src/main/java/cc/moecraft/school/database/model/base/BaseUserInfo.java
+++ GPACalc.UI/src/main/java/cc/moecraft/school/database/model/base/BaseUserInfo.java
@@ -25,20 +25,20 @@ public abstract class BaseUserInfo<M extends BaseUserInfo<M>> extends JbootModel
 		return getStr("google_sub");
 	}
 
-	public void setRegisterDate(java.util.Date registerDate) {
-		set("register_date", registerDate);
-	}
-	
-	public java.util.Date getRegisterDate() {
-		return get("register_date");
-	}
-
 	public void setDiscordId(java.lang.String discordId) {
 		set("discord_id", discordId);
 	}
 	
 	public java.lang.String getDiscordId() {
 		return getStr("discord_id");
+	}
+
+	public void setRegisterDate(java.util.Date registerDate) {
+		set("register_date", registerDate);
+	}
+	
+	public java.util.Date getRegisterDate() {
+		return get("register_date");
 	}
 
 	public void setGradingProfile(java.lang.String gradingProfile) {
