779a3a2a2b8bf074bb16f9f08e07fb9e39bb04a1
Hykilpikonna <admin@moecraft.cc>
2018-10-27 15:33:31 -0400

    [+] Api Utils class

---
+++ GPACalc.UI/src/main/java/cc/moecraft/school/utils/ApiUtils.java
@@ -0,0 +1,19 @@
+package cc.moecraft.school.utils;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonSyntaxException;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/27 创建!
+ * Created by Hykilpikonna on 2018/10/27!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class ApiUtils
+{
+}
