0f0f7fc2b49ad5f2657ac35effca89806de8a25e
Hykilpikonna <admin@moecraft.cc>
2018-10-27 15:49:33 -0400

    [+] Add service constants for database models

---
+++ GPACalc.UI/src/main/java/cc/moecraft/school/database/service/Service.java
@@ -0,0 +1,21 @@
+package cc.moecraft.school.database.service;
+
+import cc.moecraft.school.database.model.TimeMap;
+import cc.moecraft.school.database.model.UserInfo;
+import cc.moecraft.school.database.model.UserProfiles;
+import io.jboot.JbootServiceBase;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/27 创建!
+ * Created by Hykilpikonna on 2018/10/27!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class Service
+{
+    public static final JbootServiceBase<TimeMap> TIME_MAP = new JbootServiceBase<>();
+    public static final JbootServiceBase<UserInfo> USER_INFO = new JbootServiceBase<>();
+    public static final JbootServiceBase<UserProfiles> USER_PROFILES = new JbootServiceBase<>();
+}
