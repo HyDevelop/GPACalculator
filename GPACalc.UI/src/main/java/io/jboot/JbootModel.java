e14b00c767681e1c8746d739db4dd2302eae3c36
Hykilpikonna <admin@moecraft.cc>
2018-10-27 18:32:58 -0400

    [F] Fix javadocs

--- GPACalc.UI/src/main/java/io/jboot/JbootModel.java
+++ GPACalc.UI/src/main/java/io/jboot/JbootModel.java
@@ -1,21 +1,43 @@
+/*
+ * Copyright (c) 2015-2018, Michael Yang 杨福海 (fuhai999@gmail.com).
+ * <p>
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.jboot;
 
 import com.jfinal.core.JFinal;
 import com.jfinal.plugin.activerecord.Model;
 import com.jfinal.plugin.activerecord.Page;
 import com.jfinal.plugin.activerecord.Table;
+import com.jfinal.plugin.activerecord.dialect.*;
 
 import java.util.*;
 
+@SuppressWarnings({"serial", "unchecked"})
 public class JbootModel<M extends JbootModel<M>> extends Model<M>
 {
     public static final String AUTO_COPY_MODEL = "_auto_copy_model_";
     private transient Table table;
     private transient String primaryKey;
     private transient Class<?> primaryType;
+    private IJbootModelDialect jbootDialect;
 
     /**
-     * 复制一个新的model     * 主要是用在 从缓存取出数据的时候，如果直接修改，在ehcache会抛异常     * 如果要对model进行修改，可以先copy一份新的，然后再修改     *     * @return
+     * 复制一个新的model
+     * 主要是用在 从缓存取出数据的时候，如果直接修改，在ehcache会抛异常
+     * 如果要对model进行修改，可以先copy一份新的，然后再修改
+     *
+     * @return 结果
      */
     public M copy()
     {
@@ -24,7 +46,8 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
         {
             m = (M) _getUsefulClass().newInstance();
             m.put(_getAttrs());
-        } catch (Throwable e)
+        }
+        catch (Throwable e)
         {
             e.printStackTrace();
         }
@@ -32,7 +55,11 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
     }
 
     /**
-     * 在 RPC 传输的时候，通过 Controller 传入到Service     * 不同的序列化方案 可能导致 getModifyFlag 并未设置，可能造成无法保存到数据库     * 因此需要 通过这个方法 拷贝数据库对于字段，然后再进行更新或保存     *     * @return
+     * 在 RPC 传输的时候，通过 Controller 传入到Service
+     * 不同的序列化方案 可能导致 getModifyFlag 并未设置，可能造成无法保存到数据库
+     * 因此需要 通过这个方法 拷贝数据库对于字段，然后再进行更新或保存
+     *
+     * @return 结果
      */
     public M copyModel()
     {
@@ -50,7 +77,8 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
                     m.set(attrKey, o);
                 }
             }
-        } catch (Throwable e)
+        }
+        catch (Throwable e)
         {
             e.printStackTrace();
         }
@@ -58,7 +86,10 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
     }
 
     /**
-     * 修复 jfinal use 可能造成的线程安全问题     *     * @param configName     * @return
+     * 修复 jfinal use 可能造成的线程安全问题
+     *
+     * @param configName Config Name
+     * @return 结果
      */
     @Override
     public M use(String configName)
@@ -67,6 +98,7 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
         if (m == null)
         {
             m = this.copy().superUse(configName);
+
             this.put("__ds__" + configName, m);
         }
         return m;
@@ -78,42 +110,44 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
     }
 
     /**
-     * 更新或者保存     * 有主键就更新，没有就保存     *     * @return
+     * 更新或者保存
+     * 有主键就更新，没有就保存
+     *
+     * @return 结果
      */
     public boolean saveOrUpdate()
     {
         if (null == get(_getPrimaryKey()))
-        {
             return this.save();
-        }
         return this.update();
     }
 
     /**
-     * 保存数据     *     * @return
+     * 保存数据
+     *
+     * @return 结果
      */
     @Override
     public boolean save()
     {
         boolean needInitPrimaryKey = (String.class == _getPrimaryType() && null == get(_getPrimaryKey()));
+
         if (needInitPrimaryKey)
-        {
             set(_getPrimaryKey(), generatePrimaryValue());
-        }
-        boolean saveSuccess = false;
+
+        boolean saveSuccess;
+
         Boolean autoCopyModel = get(AUTO_COPY_MODEL);
-        if (autoCopyModel != null && autoCopyModel == true)
+
+        if (autoCopyModel != null && autoCopyModel)
         {
             M copyModel = copyModel();
             saveSuccess = copyModel.superSave();
+
             if (saveSuccess && !needInitPrimaryKey)
-            {
                 this.set(_getPrimaryKey(), copyModel.get(_getPrimaryKey()));
-            }
-        } else
-        {
-            saveSuccess = this.superSave();
         }
+        else saveSuccess = this.superSave();
         return saveSuccess;
     }
 
@@ -128,13 +162,15 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
     }
 
     /**
-     * 更新     *     * @return
+     * 更新
+     *
+     * @return 结果
      */
     @Override
     public boolean update()
     {
         Boolean autoCopyModel = get(AUTO_COPY_MODEL);
-        return (autoCopyModel != null && autoCopyModel == true) ? copyModel().updateNormal() : this.updateNormal();
+        return (autoCopyModel != null && autoCopyModel) ? copyModel().updateNormal() : this.updateNormal();
     }
 
     boolean updateNormal()
@@ -163,30 +199,41 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
     }
 
     /**
-     * 根据列名和值，查找1条数据     *     * @param column     * @param value     * @return
+     * 根据列名和值，查找1条数据
+     *
+     * @param column Column
+     * @param value  Value
+     * @return 结果
      */
     public M findFirstByColumn(String column, Object value)
     {
-        String sql = getDialect().forFindByColumns(_getTableName(), "*", Columns.create(column, value).getList(), null, 1);
+        String sql = getJbootDialect().forFindByColumns(_getTableName(), "*", Columns.create(column, value).getList(), null, 1);
         return findFirst(sql, value);
     }
 
     /**
-     * 根据 列和值 查询1条数据     *     * @param column     * @return
+     * 根据 列和值 查询1条数据
+     *
+     * @param column Column
+     * @return 结果
      */
     public M findFirstByColumn(Column column)
     {
-        String sql = getDialect().forFindByColumns(_getTableName(), "*", Columns.create(column).getList(), null, 1);
+        String sql = getJbootDialect().forFindByColumns(_getTableName(), "*", Columns.create(column).getList(), null, 1);
         return findFirst(sql, column.getValue());
     }
 
     /**
-     * 根据 多列和值，查询1条数据     *     * @param columns     * @return
+     * 根据 多列和值，查询1条数据
+     *
+     * @param columns Columns
+     * @return 结果
      */
     public M findFirstByColumns(Columns columns)
     {
-        String sql = getDialect().forFindByColumns(_getTableName(), "*", columns.getList(), null, 1);
-        LinkedList<Object> params = new LinkedList<Object>();
+        String sql = getJbootDialect().forFindByColumns(_getTableName(), "*", columns.getList(), null, 1);
+        LinkedList<Object> params = new LinkedList<>();
+
         if (ArrayUtils.isNotEmpty(columns.getList()))
         {
             for (Column column : columns.getList())
@@ -198,16 +245,23 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
     }
 
     /**
-     * 查找全部数据     *     * @return
+     * 查找全部数据
+     *
+     * @return 结果
      */
     public List<M> findAll()
     {
-        String sql = getDialect().forFindByColumns(_getTableName(), "*", null, null, null);
+        String sql = getJbootDialect().forFindByColumns(_getTableName(), "*", null, null, null);
         return find(sql);
     }
 
     /**
-     * 根据列名和值 查询一个列表     *     * @param column     * @param value     * @param count  最多查询多少条数据     * @return
+     * 根据列名和值 查询一个列表
+     *
+     * @param column Column
+     * @param value  Value
+     * @param count  最多查询多少条数据
+     * @return 结果
      */
     public List<M> findListByColumn(String column, Object value, Integer count)
     {
@@ -217,7 +271,11 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
     }
 
     /**
-     * 根据 列信息 查找数据列表     *     * @param column     * @param count     * @return
+     * 根据 列信息 查找数据列表
+     *
+     * @param column Column
+     * @param count  Count
+     * @return 结果
      */
     public List<M> findListByColumn(Column column, Integer count)
     {
@@ -270,11 +328,17 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
     }
 
     /**
-     * 根据列信心查询列表     *     * @param columns     * @param orderBy     * @param count     * @return
+     * 根据列信心查询列表
+     *
+     * @param columns Columns
+     * @param orderBy Order By
+     * @param count   Count
+     * @return 结果
      */
     public List<M> findListByColumns(List<Column> columns, String orderBy, Integer count)
     {
-        LinkedList<Object> params = new LinkedList<Object>();
+        LinkedList<Object> params = new LinkedList<>();
+
         if (ArrayUtils.isNotEmpty(columns))
         {
             for (Column column : columns)
@@ -282,12 +346,17 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
                 params.add(column.getValue());
             }
         }
-        String sql = getDialect().forFindByColumns(_getTableName(), "*", columns, orderBy, count);
+
+        String sql = getJbootDialect().forFindByColumns(_getTableName(), "*", columns, orderBy, count);
         return params.isEmpty() ? find(sql) : find(sql, params.toArray());
     }
 
     /**
-     * 分页查询数据     *     * @param pageNumber     * @param pageSize     * @return
+     * 分页查询数据
+     *
+     * @param pageNumber Page Number
+     * @param pageSize   Page Size
+     * @return 结果
      */
     public Page<M> paginate(int pageNumber, int pageSize)
     {
@@ -295,7 +364,11 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
     }
 
     /**
-     * 分页查询数据     *     * @param pageNumber     * @param pageSize     * @return
+     * 分页查询数据
+     *
+     * @param pageNumber Page Number
+     * @param pageSize   Page Size
+     * @return 结果
      */
     public Page<M> paginate(int pageNumber, int pageSize, String orderBy)
     {
@@ -303,7 +376,12 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
     }
 
     /**
-     * 根据某列信息，分页查询数据     *     * @param pageNumber     * @param pageSize     * @param column     * @return
+     * 根据某列信息，分页查询数据
+     *
+     * @param pageNumber Page Number
+     * @param pageSize   Page Size
+     * @param column     Column
+     * @return 结果
      */
     public Page<M> paginateByColumn(int pageNumber, int pageSize, Column column)
     {
@@ -311,7 +389,12 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
     }
 
     /**
-     * 根据某列信息，分页查询数据     *     * @param pageNumber     * @param pageSize     * @param column     * @return
+     * 根据某列信息，分页查询数据
+     *
+     * @param pageNumber Page Number
+     * @param pageSize   Page Size
+     * @param column     Column
+     * @return 结果
      */
     public Page<M> paginateByColumn(int pageNumber, int pageSize, Column column, String orderBy)
     {
@@ -319,7 +402,12 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
     }
 
     /**
-     * 根据列信息，分页查询数据     *     * @param pageNumber     * @param pageSize     * @param columns     * @return
+     * 根据列信息，分页查询数据
+     *
+     * @param pageNumber Page Number
+     * @param pageSize   Page Size
+     * @param columns    Columns
+     * @return 结果
      */
     public Page<M> paginateByColumns(int pageNumber, int pageSize, List<Column> columns)
     {
@@ -327,13 +415,21 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
     }
 
     /**
-     * 根据列信息，分页查询数据     *     * @param pageNumber     * @param pageSize     * @param columns     * @param orderBy     * @return
+     * 根据列信息，分页查询数据
+     *
+     * @param pageNumber Page Number
+     * @param pageSize   Page Size
+     * @param columns    Columns
+     * @param orderBy    Order By
+     * @return 结果
      */
     public Page<M> paginateByColumns(int pageNumber, int pageSize, List<Column> columns, String orderBy)
     {
-        String selectPartSql = getDialect().forPaginateSelect("*");
-        String fromPartSql = getDialect().forPaginateFrom(_getTableName(), columns, orderBy);
-        LinkedList<Object> params = new LinkedList<Object>();
+        String selectPartSql = getJbootDialect().forPaginateSelect("*");
+        String fromPartSql = getJbootDialect().forPaginateFrom(_getTableName(), columns, orderBy);
+
+        LinkedList<Object> params = new LinkedList<>();
+
         if (ArrayUtils.isNotEmpty(columns))
         {
             for (Column column : columns)
@@ -341,7 +437,8 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
                 params.add(column.getValue());
             }
         }
-        return params.isEmpty() ? paginate(pageNumber, pageSize, selectPartSql, fromPartSql) : paginate(pageNumber, pageSize, selectPartSql, fromPartSql, params.toArray());
+        return params.isEmpty() ? paginate(pageNumber, pageSize, selectPartSql, fromPartSql)
+                : paginate(pageNumber, pageSize, selectPartSql, fromPartSql, params.toArray());
     }
 
     protected String _getTableName()
@@ -361,7 +458,8 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
             table = super._getTable();
             if (table == null && validateNull)
             {
-                throw new RuntimeException(String.format("class %s can not mapping to database table, maybe cannot connection to database or not use correct datasource, " + "please check jboot.properties or correct config @Table(datasource=xxx) if you use multi datasource.", _getUsefulClass().getName()));
+                throw new RuntimeException(String.format("class %s can not mapping to database table, maybe cannot connection to database or not use correct datasource, " +
+                        "please check jboot.properties or correct config @Table(datasource=xxx) if you use multi datasource.", _getUsefulClass().getName()));
             }
         }
         return table;
@@ -369,25 +467,21 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
 
     protected String _getPrimaryKey()
     {
-        if (primaryKey != null)
-        {
-            return primaryKey;
-        }
+        if (primaryKey != null) return primaryKey;
+
         String[] primaryKeys = _getTable(true).getPrimaryKey();
-        if (null != primaryKeys && primaryKeys.length == 1)
-        {
-            primaryKey = primaryKeys[0];
-        }
+
+        if (null != primaryKeys && primaryKeys.length == 1) primaryKey = primaryKeys[0];
+
         JbootAssert.assertTrue(primaryKey != null, String.format("get PrimaryKey is error in[%s]", getClass()));
+
         return primaryKey;
     }
 
     protected Class<?> _getPrimaryType()
     {
         if (primaryType == null)
-        {
             primaryType = _getTable(true).getColumnType(_getPrimaryKey());
-        }
         return primaryType;
     }
 
@@ -461,27 +555,19 @@ public class JbootModel<M extends JbootModel<M>> extends Model<M>
     private void debugPrintParas(Object... objects)
     {
         if (JFinal.me().getConstants().getDevMode())
-        {
             System.out.println("\r\n---------------Paras: " + Arrays.toString(objects) + "----------------");
-        }
     }
 
     @Override
     public boolean equals(Object o)
     {
-        if (o == null)
-        {
-            return false;
-        }
-        if (!(o instanceof JbootModel))
-        {
-            return false;
-        }
+        if (o == null) return false;
+        if (!(o instanceof JbootModel)) return false;
+
         Object id = ((JbootModel) o).get(_getPrimaryKey());
-        if (id == null)
-        {
-            return false;
-        }
+
+        if (id == null) return false;
         return id.equals(get(_getPrimaryKey()));
     }
+
 }
