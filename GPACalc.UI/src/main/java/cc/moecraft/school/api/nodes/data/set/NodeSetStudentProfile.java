f0563de37971f4b29aa39a7bbab4f1cfe3188429
Hykilpikonna <admin@moecraft.cc>
2018-10-27 19:58:21 -0400

    [+] Node to set student profile

---
+++ GPACalc.UI/src/main/java/cc/moecraft/school/api/nodes/data/set/NodeSetStudentProfile.java
@@ -0,0 +1,44 @@
+package cc.moecraft.school.api.nodes.data.set;
+
+import cc.moecraft.school.api.ApiNode;
+import cc.moecraft.school.profile.student.StudentProfile;
+import com.google.api.client.googleapis.auth.oauth2.GoogleIdToken;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static cc.moecraft.school.database.DatabaseUtils.updateProfile;
+import static cc.moecraft.school.utils.JsonUtils.JsonParsingException;
+import static cc.moecraft.school.utils.JsonUtils.parseJson;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/27 创建!
+ * Created by Hykilpikonna on 2018/10/27!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class NodeSetStudentProfile implements ApiNode
+{
+    @Override
+    public String nodeName()
+    {
+        return "data.set.student-profile";
+    }
+
+    @Override
+    public String process(HttpServletRequest request, GoogleIdToken token, String content)
+    {
+        try
+        {
+            StudentProfile studentProfile = parseJson(content, StudentProfile.class);
+            updateProfile(token.getPayload().getSubject(), studentProfile);
+
+            return "Success";
+        }
+        catch (JsonParsingException e)
+        {
+            return e.getMessage();
+        }
+    }
+}
