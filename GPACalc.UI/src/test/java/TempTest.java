264332f869d6f4c137f2ac36b7cb3060dab2fc39
Hykilpikonna <admin@moecraft.cc>
2018-10-28 10:58:24 -0400

    [T] Test json of grades object

---
+++ GPACalc.UI/src/test/java/TempTest.java
@@ -0,0 +1,28 @@
+import cc.moecraft.school.Grades;
+
+import static cc.moecraft.school.Constants.GSON;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/10/27 创建!
+ * Created by Hykilpikonna on 2018/10/27!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class TempTest
+{
+    public static void main(String[] args)
+    {
+        Grades grades = new Grades()
+                .putGrade("English 2 A", "A-")
+                .putGrade("Algebra 2 H", "A+")
+                .putGrade("Chemistry 1 H", "B+")
+                .putGrade("Modern World History CP", "A+")
+                .putGrade("Early Church CP", "B+")
+                .putGrade("AP Computer Science A", "A+");
+
+        System.out.println(GSON.toJson(grades));
+    }
+}
+
