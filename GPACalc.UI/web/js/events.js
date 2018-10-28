baca7f21c83dc0d9d0a2a2d117310ede1b65f568
Hykilpikonna <admin@moecraft.cc>
2018-10-28 13:24:31 -0400

    [+] Register message close

--- GPACalc.UI/web/js/events.js
+++ GPACalc.UI/web/js/events.js
@@ -26,3 +26,8 @@ function checkAZ(id)
         element.parent().removeClass("error");
     }
 }
+
+$('.message .close').on('click', function()
+{
+    $(this).closest('.message').transition('fade');
+});
