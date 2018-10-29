ea42fb3da6c55e1be6869b33aa3741a48354c862
Hykilpikonna <admin@moecraft.cc>
2018-10-28 21:38:49 -0400

    [+] Add message function

--- GPACalc.UI/web/js/message.js
+++ GPACalc.UI/web/js/message.js
@@ -1,3 +1,12 @@
 class Msg {}
 var msg = new Msg();
 
+msg.addMsg = function (format, title, ... lines)
+{
+    var linesText = "";
+    var div = $("#messages-div");
+
+    lines.forEach(text => linesText += "<li>" + text + "</li>");
+    div.append(format.replace(/%title%/g, title).replace(/%lines%/g, linesText));
+};
+
