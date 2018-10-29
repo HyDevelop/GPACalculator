c59f482e31d83004c707c691b2abe5bcb06ee0b6
Hykilpikonna <admin@moecraft.cc>
2018-10-28 21:39:46 -0400

    [+] Message for success

--- GPACalc.UI/web/js/message.js
+++ GPACalc.UI/web/js/message.js
@@ -1,6 +1,11 @@
 class Msg {}
 var msg = new Msg();
 
+msg.success = function (title, ... lines)
+{
+    msg.addMsg(messageSuccess, title, lines);
+};
+
 msg.addMsg = function (format, title, ... lines)
 {
     var linesText = "";
