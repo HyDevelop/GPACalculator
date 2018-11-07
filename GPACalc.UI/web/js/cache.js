8602db0154ede53e1a9cf5fc4697ab64bab1e18d
Hykilpikonna <admin@moecraft.cc>
2018-11-07 18:52:15 -0500

    [+] Load cache from server

--- GPACalc.UI/web/js/cache.js
+++ GPACalc.UI/web/js/cache.js
@@ -1,3 +1,22 @@
 class Cache {}
 var cache = new Cache();
 
+/**
+ * Load profiles from server and cache them.
+ *
+ * @param callback Callback function after loaded
+ */
+cache.loadFromServer = function(callback)
+{
+    send("data.get.student-profile", "", function (studentProfile)
+    {
+        cache.studentProfile = JSON.parse(studentProfile);
+        if (cache.gradingProfile != null) callback();
+    });
+
+    send("data.get.grading-profile", "", function (gradingProfile)
+    {
+        cache.gradingProfile = JSON.parse(gradingProfile);
+        if (cache.studentProfile != null) callback();
+    });
+};
