a138b8a324c97eab1d51ccba5dd9994ea010a83a
Hykilpikonna <admin@moecraft.cc>
2018-11-08 07:17:32 -0500

    [+] Veracross pojo interface

---
+++ GPACalc.Core/src/main/java/cc/moecraft/school/veracross/pojo/VeracrossPojo.java
@@ -0,0 +1,13 @@
+package cc.moecraft.school.veracross.pojo;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/11/08 创建!
+ * Created by Hykilpikonna on 2018/11/08!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public interface VeracrossPojo
+{
+}
