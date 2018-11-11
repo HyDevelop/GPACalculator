7a66ceaf6580665ee557e85c896cc8af3a96c927
Hykilpikonna <admin@moecraft.cc>
2018-11-11 13:56:41 -0500

    [+] Attribute constants

---
+++ GPACalc.Core/src/main/java/cc/moecraft/school/veracross/Attributes.java
@@ -0,0 +1,15 @@
+package cc.moecraft.school.veracross;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/11/11 创建!
+ * Created by Hykilpikonna on 2018/11/11!
+ * Github: https://github.com/hykilpikonna
+ * QQ: admin@moecraft.cc -OR- 871674895
+ *
+ * @author Hykilpikonna
+ */
+public class Attributes
+{
+    public static final String INNER_HTML = "innerHTML";
+    public static final String HREF = "href";
+}
