e1b26d5032ff3a8500e936fa7a97bee674a6f3d3
Hykilpikonna <admin@moecraft.cc>
2018-11-15 16:05:50 -0500

    [M] Move sendDebug() to api.sendDebug()

--- GPACalc.UI/web/js/app.js
+++ GPACalc.UI/web/js/app.js
@@ -5,42 +5,12 @@ var banner =
     "               v1.0.0.0 By Hykilpikonna";
 
 /**
- * Send and debug the response to console.
- *
- * @param apiNode Node
- * @param content Content
- */
-function sendDebug(apiNode, content)
-{
-    api.send(apiNode, content, response => console.log(response), error =>
-    {
-        console.log("Error: " + error);
-        return false;
-    })
-}
-
-/**
  * Called when login is verified.
  */
 function onLoginVerified()
 {
     // Reload if signed in for the first time.
     if (UIkit.modal("#modal-login").isToggled()) reload();
-
-    /*
-    // Useful data for your client-side scripts:
-    var profile = google.user.getBasicProfile();
-    console.log("ID: " + profile.getId());
-    console.log('Full Name: ' + profile.getName());
-    console.log('Given Name: ' + profile.getGivenName());
-    console.log('Family Name: ' + profile.getFamilyName());
-    console.log("Image URL: " + profile.getImageUrl());
-    console.log("Email: " + profile.getEmail());
-
-    // The ID token you need to pass to your backend:
-    var id_token = google.user.getAuthResponse().id_token;
-    console.log("ID Token: " + id_token);
-    */
 
     // Show sign off button
     $(".auth-signOut").show();
