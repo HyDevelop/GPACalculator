da9577b68da08a13ff9c1aebfc99f68c224a49a1
Hykilpikonna <admin@moecraft.cc>
2018-11-25 18:02:48 -0500

    [+] Get param as boolean

--- GPACalc.UI/src/main/java/cc/moecraft/school/utils/RequestUtils.java
+++ GPACalc.UI/src/main/java/cc/moecraft/school/utils/RequestUtils.java
@@ -27,4 +27,17 @@ public class RequestUtils
         String value = request.getHeader(key);
         return value == null ? def : value;
     }
+
+    /**
+     * Get param from header.
+     *
+     * @param request Request
+     * @param key Header key
+     * @param def Default value
+     * @return Param, or default if it doesn't exist.
+     */
+    public static boolean getParam(HttpServletRequest request, String key, boolean def)
+    {
+        return parseBoolean(getParam(request, key, def + ""));
+    }
 }
