e84d11fad42223ea7d7614ba37ed2b5d10f3ef1b
Hykilpikonna <admin@moecraft.cc>
2018-11-25 18:02:37 -0500

    [+] Method to get header param with default value

---
+++ GPACalc.UI/src/main/java/cc/moecraft/school/utils/RequestUtils.java
@@ -0,0 +1,30 @@
+package cc.moecraft.school.utils;
+
+import javax.servlet.http.HttpServletRequest;
+
+import static java.lang.Boolean.*;
+
+/**
+ * 此类由 Hykilpikonna 在 2018/11/25 创建!
+ * Created by Hykilpikonna on 2018/11/25!
+ * Github: https://github.com/hykilpikonna
+ * Meow!
+ *
+ * @author Hykilpikonna
+ */
+public class RequestUtils
+{
+    /**
+     * Get param from header.
+     *
+     * @param request Request
+     * @param key Header key
+     * @param def Default value
+     * @return Param, or default if it doesn't exist.
+     */
+    public static String getParam(HttpServletRequest request, String key, String def)
+    {
+        String value = request.getHeader(key);
+        return value == null ? def : value;
+    }
+}
