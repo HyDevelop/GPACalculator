e0aa64ef3d6344cae0eff8366f7276a3046fa109
Hykilpikonna <admin@moecraft.cc>
2019-03-26 16:45:33 -0400

    [+] Create signOut() method

--- GPACalc.UI/web/vinfo/js/google-new.js
+++ GPACalc.UI/web/vinfo/js/google-new.js
@@ -6,6 +6,17 @@ function Google()
     // gAPI and Auth2 Instance (Initialized on init())
     var g = gapi;
     var a;
+
+    /**
+     * Sign out.
+     */
+    this.signOut = function()
+    {
+        a.signOut().then(function ()
+        {
+            console.log('[GoogleApi] Signed out.');
+        });
+    };
 }
 
 const google = new Google();
