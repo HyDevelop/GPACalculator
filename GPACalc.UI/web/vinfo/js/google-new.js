314e088b0fc64dd127b490239fb12d9dd918d8d0
Hykilpikonna <admin@moecraft.cc>
2019-03-26 16:43:23 -0400

    [+] Create function-class for Google authentication

---
+++ GPACalc.UI/web/vinfo/js/google-new.js
@@ -0,0 +1,13 @@
+/**
+ * Class related to the Google API and authentication.
+ */
+function Google()
+{
+}
+
+const google = new Google();
+
+/**
+ * Run on load.
+ */
+$(document).ready(google.init());
