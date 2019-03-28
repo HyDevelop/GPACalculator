f49542a9f7e5dabdca44e53d029c1f90680f0357
Hykilpikonna <admin@moecraft.cc>
2019-03-28 16:56:48 -0400

    [M] Move send() and sendDebug() into the api class.

--- GPACalc.UI/web/shared/js/api.js
+++ GPACalc.UI/web/shared/js/api.js
@@ -8,76 +8,74 @@ function GPACalcApi()
      */
     var url = this.url = constants.api_url;
 
+    /**
+     * Send a request to api.
+     *
+     * @param apiNode Node of the api.
+     * @param content Content in String.
+     * @param callback Function to callback after.
+     *          function(return result)
+     * @param errorCallback Callback when there is an error.
+     *          function(error message) that returns a boolean that decides whether to send error or not.
+     * @param params Header parameter key value pairs
+     */
+    this.send = function (apiNode, content, callback, errorCallback, ... params)
+    {
+        if (google.user == null)
+        {
+            msg.error("Error: You must be logged in to do that.", "No google login is detected.");
+            return;
+        }
 
+        var request = new XMLHttpRequest();
+        request.open("POST", api.url, true);
+        request.setRequestHeader("node", apiNode);
+        request.setRequestHeader("token", google.user.getAuthResponse().id_token);
+        request.onreadystatechange = function ()
+        {
+            if (request.readyState === 4 && request.status === 200)
+            {
+                var text = request.responseText;
+
+                // Error
+                if (text.toLowerCase().startsWith("error"))
+                {
+                    // Error callback, returns boolean that decides whether to send error message or not.
+                    var sendErrorMessage = true;
+                    if (errorCallback != null) sendErrorMessage = errorCallback(text);
+                    if (sendErrorMessage) msg.error("Error: Data Request Failed.",
+                        "Requested API Node: " + apiNode,
+                        "Error Response: " + text);
+                    return;
+                }
+
+                // Async callback
+                callback(text);
+            }
+        };
+
+        // Set param headers.
+        if (params == null) params = [];
+        for (var i = 0; i < params.length; i += 2)
+            request.setRequestHeader(params[i], params[i + 1]);
+
+        request.send(content);
+    };
+
+    /**
+     * Send and debug the response to console.
+     *
+     * @param apiNode Node
+     * @param content Content
+     */
+    this.sendDebug = function (apiNode, content)
+    {
+        api.send(apiNode, content, response => console.log(response), error =>
+        {
+            console.log("Error: " + error);
+            return false;
+        })
+    };
 }
 
 const api = new GPACalcApi();
-
-/**
- * Send a request to api.
- *
- * @param apiNode Node of the api.
- * @param content Content in String.
- * @param callback Function to callback after.
- *          function(return result)
- * @param errorCallback Callback when there is an error.
- *          function(error message) that returns a boolean that decides whether to send error or not.
- * @param params Header parameter key value pairs
- */
-api.send = function (apiNode, content, callback, errorCallback, ... params)
-{
-    if (google.user == null)
-    {
-        msg.error("Error: You must be logged in to do that.", "No google login is detected.");
-        return;
-    }
-
-    var request = new XMLHttpRequest();
-    request.open("POST", api.url, true);
-    request.setRequestHeader("node", apiNode);
-    request.setRequestHeader("token", google.user.getAuthResponse().id_token);
-    request.onreadystatechange = function ()
-    {
-        if (request.readyState === 4 && request.status === 200)
-        {
-            var text = request.responseText;
-
-            // Error
-            if (text.toLowerCase().startsWith("error"))
-            {
-                // Error callback, returns boolean that decides whether to send error message or not.
-                var sendErrorMessage = true;
-                if (errorCallback != null) sendErrorMessage = errorCallback(text);
-                if (sendErrorMessage) msg.error("Error: Data Request Failed.",
-                    "Requested API Node: " + apiNode,
-                    "Error Response: " + text);
-                return;
-            }
-
-            // Async callback
-            callback(text);
-        }
-    };
-    
-    // Set param headers.
-    if (params == null) params = [];
-    for (var i = 0; i < params.length; i += 2)
-        request.setRequestHeader(params[i], params[i + 1]);
-
-    request.send(content);
-};
-
-/**
- * Send and debug the response to console.
- *
- * @param apiNode Node
- * @param content Content
- */
-api.sendDebug = function (apiNode, content)
-{
-    api.send(apiNode, content, response => console.log(response), error =>
-    {
-        console.log("Error: " + error);
-        return false;
-    })
-};
